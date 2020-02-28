6c135363d64576a5dc73b163e106f25c0eb0e84c
Balasubramanian <61572479+companyofbala@users.noreply.github.com>
2020-02-28 19:43:55 +0530

    Initial commit

---
+++ AgriMate/app/src/main/java/com/example/agrimate/InvesterFarm.java
@@ -0,0 +1,37 @@
+package com.example.agrimate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InvesterFarm extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inv_farm);
+        Button buttonOne = findViewById(R.id.indi);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+
+                Intent activity2Intent = new Intent(getApplicationContext(), choose.class);
+                startActivity(activity2Intent);
+            }
+        });
+
+        Button buttonTwo = findViewById(R.id.farm);
+        buttonTwo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+
+                Intent activity2Intent = new Intent(getApplicationContext(), choose.class);
+                startActivity(activity2Intent);
+            }
+        });
+
+    }
+
+}
---
+++ AgriMate/app/src/main/java/com/example/agrimate/LoginFarm.java
@@ -0,0 +1,13 @@
+package com.example.agrimate;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginFarm extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_farm);
+    }
+}
