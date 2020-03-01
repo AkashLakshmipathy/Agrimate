586f7cdfa690bf0737116118d3432a24fa11594d
Risu-swift <redrizwan007@gmail.com>
2020-03-01 10:17:34 +0530

    Investors stock

---
+++ AgriMate/app/src/main/java/com/hello/agrimate/StockHistory.java
@@ -0,0 +1,15 @@
+package com.hello.agrimate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StockHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stockhistory);
+
+    }
+}
